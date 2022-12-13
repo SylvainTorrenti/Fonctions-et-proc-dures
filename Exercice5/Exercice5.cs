18018c6ae047687b13e1d6bb1cbe3c3ae38efb7b
SylvainTorrenti <torrsyl@gmail.com>
2022-12-13 16:36:59 +0100

    exercice

---
+++ Exercice5/Exercice5.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static public double Carre(double a)
+    {
+        return a * a;
+    }
+    
+    static double Distance(double x1, double y1, double x2, double y2)
+    {
+        return Math.Sqrt(Carre(y2 - y1) + Carre(x2 - x1));
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calcul la distance entre deux points (5,6)-(4,3): " + Distance(5, 6, 4, 3));
+        Console.WriteLine("Calcul la distance entre deux points (4,3)-(5,6): " + Distance(4, 3, 5, 6));
+    }
+}
