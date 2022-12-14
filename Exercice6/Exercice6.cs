d33eef831292e2ada4a1381f00304b54e1227d69
SylvainTorrenti <torrsyl@gmail.com>
2022-12-14 09:54:04 +0100

    exercice

---
+++ Exercice6/Exercice6.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static public string Reponse(string a)
+    {
+        if (a != "Oui" && a != "Non")
+        {
+            return "Répondez par Oui ou Non";
+        }
+        else
+        {
+            return a;
+        }
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        string reponse;
+        Console.WriteLine("Etes vous marriez?");
+        reponse = Console.ReadLine();
+        Console.WriteLine(Reponse(reponse));
+        while (reponse != "Oui" && reponse != "Non")
+        {
+            Console.WriteLine("Etes vous marriez?");
+            reponse = Console.ReadLine();
+            Console.WriteLine(Reponse(reponse));
+        }
+
+    }
+}
