eeafcda2756e4c270efa82d03323472e4b19b624
SylvainTorrenti <torrsyl@gmail.com>
2022-12-15 09:47:56 +0100

    exercice

--- Exercice8/Exercice8.cs
+++ Exercice8/Exercice8.cs
@@ -1,4 +1,27 @@
 ﻿class Program
 {
-static public bool
+    public static bool EstPremier(int nb)
+    {
+        if (nb <= 3)
+        {
+            return true;
+        }
+        else
+        {
+            int SqrtEntier = (int)Math.Sqrt(nb);
+            for (int i = 2; i <= SqrtEntier; i++)
+            {
+                if (nb % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(EstPremier(2));
+        Console.WriteLine(EstPremier(5));
+        Console.WriteLine(EstPremier(6));
+        Console.WriteLine(EstPremier(25));
+    }
 }
