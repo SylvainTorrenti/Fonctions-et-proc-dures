c164dadb9b6d2464bcf251f6f31629521630d203
SylvainTorrenti <torrsyl@gmail.com>
2022-12-19 13:30:53 +0100

    exercice

---
+++ Exercice11/Exercice11.cs
@@ -0,0 +1,26 @@
+﻿//class Program
+//{
+
+//    static string Inverse(string a)
+//    {
+//        string[] b = new string[a.Length];
+//        for (int i = a.Length - 1; i > -1; i--)
+//        {
+
+//            b[i] = a[i];
+//        }
+//        return b;
+
+//    }
+//    static bool EstPalindrome(string a)
+//    {
+//        return Inverse(a) == a;
+//    }
+//    static void Main(string[] args)
+//    {
+//        Inverse("Bonjour");
+//        Console.WriteLine("");
+//        Inverse("WINDOWS");
+//    }
+
+//}
