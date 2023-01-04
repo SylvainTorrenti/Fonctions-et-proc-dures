f7e671224a087df216c45b551cf755e9fa0df942
SylvainTorrenti <torrsyl@gmail.com>
2023-01-04 11:59:01 +0100

    exercice

--- Exercice10/Exercice10.cs
+++ Exercice10/Exercice10.cs
@@ -14,20 +14,19 @@ int ConvertirEnDecimalString(string Nombre)
     return Convert.ToInt32(Nombre, 2);
 }
 
-//Console.WriteLine(ConvertirEnBinaire(2));
-//Console.WriteLine(ConvertirEnBinaire(8));
-//Console.WriteLine(ConvertirEnBinaire(9));
-//Console.WriteLine(ConvertirEnBinaire(36));
-//Console.WriteLine("");
-//Console.WriteLine(ConvertirEnDecimalInt(10));
-//Console.WriteLine(ConvertirEnDecimalInt(1000));
-//Console.WriteLine(ConvertirEnDecimalInt(1001));
-//Console.WriteLine(ConvertirEnDecimalInt(100100));
-//Console.WriteLine("");
-//Console.WriteLine(ConvertirEnDecimalString("10"));
-//Console.WriteLine(ConvertirEnDecimalString("1000"));
-//Console.WriteLine(ConvertirEnDecimalString("1001"));
-//Console.WriteLine(ConvertirEnDecimalString("100100"));
+Console.WriteLine(ConvertirEnBinaire(2));
+Console.WriteLine(ConvertirEnBinaire(8));
+Console.WriteLine(ConvertirEnBinaire(9));
+Console.WriteLine(ConvertirEnBinaire(36));
+Console.WriteLine("");
+Console.WriteLine(ConvertirEnDecimalInt(10));
+Console.WriteLine(ConvertirEnDecimalInt(1000));
+Console.WriteLine(ConvertirEnDecimalInt(1001));
+Console.WriteLine(ConvertirEnDecimalInt(100100));
+Console.WriteLine("");
+Console.WriteLine(ConvertirEnDecimalString("10"));
+Console.WriteLine(ConvertirEnDecimalString("1000"));
+Console.WriteLine(ConvertirEnDecimalString("1001"));
+Console.WriteLine(ConvertirEnDecimalString("100100"));
 
-Console.WriteLine(ConvertirEnDecimalInt(-5));
 
