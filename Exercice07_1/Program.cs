1ba7d7a0dbb49b56a5b551255198632af590fade
SylvainTorrenti <torrsyl@gmail.com>
2023-01-04 09:37:46 +0100

    exercice

---
+++ Exercice07_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Fonction indiquant si un nombre est pair ou non
+bool EstPair(int Entier)
+{
+    return Entier % 2 == 0;
+}
+
+Console.WriteLine(EstPair(2));
+Console.WriteLine(EstPair(3));
+bool Rep = EstPair(4);
+Console.WriteLine(Rep);
+
+if (EstPair(5))
+{
+    Console.WriteLine("Le nombre est pair");
+}
+else
+{
+    Console.WriteLine("le nombre est impair");
+}
